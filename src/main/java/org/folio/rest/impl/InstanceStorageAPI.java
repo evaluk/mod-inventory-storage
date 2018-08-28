9d7e6f287ebe2542e0de5b3e68d3d74f8f70c5ea
Marc Johnson <marc@k-int.co.uk>
2018-08-28 09:58:41 +0100

    Use criteria instead of CQL when finding instance by ID

--- src/main/java/org/folio/rest/impl/InstanceStorageAPI.java
+++ src/main/java/org/folio/rest/impl/InstanceStorageAPI.java
@@ -1,26 +1,24 @@
 package org.folio.rest.impl;
 
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Context;
-import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.core.Response;
+
 import org.folio.rest.jaxrs.model.Instance;
 import org.folio.rest.jaxrs.model.Instances;
 import org.folio.rest.jaxrs.model.MarcJson;
 import org.folio.rest.jaxrs.resource.InstanceStorageResource;
+import org.folio.rest.persist.Criteria.Criteria;
+import org.folio.rest.persist.Criteria.Criterion;
 import org.folio.rest.persist.Criteria.Limit;
 import org.folio.rest.persist.Criteria.Offset;
 import org.folio.rest.persist.PgExceptionUtil;
@@ -30,6 +28,13 @@ import org.folio.rest.tools.utils.OutStream;
 import org.folio.rest.tools.utils.TenantTool;
 import org.z3950.zing.cql.cql2pgjson.CQL2PgJSON;
 import org.z3950.zing.cql.cql2pgjson.FieldException;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 public class InstanceStorageAPI implements InstanceStorageResource {
 
@@ -282,15 +287,17 @@ public class InstanceStorageAPI implements InstanceStorageResource {
       PostgresClient postgresClient = PostgresClient.getInstance(
         vertxContext.owner(), TenantTool.calculateTenantId(tenantId));
 
-      String[] fieldList = {"*"};
+      Criteria criteria = new Criteria()
+        .addField("_id")
+        .setJSONB(false)
+        .setOperation("=")
+        .setValue("'" + instanceId + "'");
 
-      CQLWrapper cql = handleCQL(String.format("id==%s", instanceId), 1, 0);
-
-      log.info(String.format("SQL generated from CQL: %s", cql.toString()));
+      Criterion criterion = new Criterion().addCriterion(criteria);
 
       vertxContext.runOnContext(v -> {
         try {
-          postgresClient.get(tableName, Instance.class, fieldList, cql, true, false,
+          postgresClient.get(tableName, Instance.class, criterion, false,
             reply -> {
               try {
                 if (reply.succeeded()) {
@@ -391,11 +398,15 @@ public class InstanceStorageAPI implements InstanceStorageResource {
 
       vertxContext.runOnContext(v -> {
         try {
-          String[] fieldList = {"*"};
+          Criteria criteria = new Criteria()
+            .addField("_id")
+            .setJSONB(false)
+            .setOperation("=")
+            .setValue("'" + instanceId + "'");
 
-          CQLWrapper cql = handleCQL(String.format("id==%s", instanceId), 1, 0);
+          Criterion criterion = new Criterion().addCriterion(criteria);
 
-          postgresClient.get(tableName, Instance.class, fieldList, cql, true, false,
+          postgresClient.get(tableName, Instance.class, criterion, false,
             reply -> {
               if(reply.succeeded()) {
                 List<Instance> instancesList = (List<Instance>) reply.result().getResults();
